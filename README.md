5b9ab12713575ef963b480fb69ad45c7805a2d20
Dean Kooiman <dean.kooiman@cybergrx.com>
2020-10-26 19:25:07 -0600

    Rename PR using configurable regex and format (#2)
    * Add configuration for regex and format
    * Use ncc
    * Convert to typescript
    * Update readme

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# pr-rename-action
-Rename a PR on open using a regex
+# PR Rename Javascript Action
+This action automatically renames a PR using a regex with named groups and [custom format](https://www.npmjs.com/package/string-format).
+
+## Inputs
+
+### `branch-regex`
+
+**Required** Regex with named groups used to process the PR's source branch.
+
+### `title-format`
+
+**Required** String format using the `branch-regex` named groups that will construct a clean PR title.
+
+There are 2 special transforms available
+1. `{group!upper}` - will transform the group to upper case.
+1. `{group!normalize}` - will transform the group, replacing non words with space, converting to title case, and trimming the resulting string.
+
+## Outputs
+
+### `matches`
+
+The groups that were matched by the `branch-regex`.
+
+## Example usage
+
+It is recommended that you only run this action when a PR is opened so you do not overwrite a custom title.
+```
+on:
+  pull_request:
+    types: [opened, synchronize]
+```
+
+Example step
+```
+name: Rename PR
+uses: CyberGRX/pr-rename-action@v1.0
+id: pr-auto-namer
+env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+with:
+    branch-regex: '(?<prefix>.*?)(?<team>[\d\w]+)-(?<ticket>\d+)(?<postfix>.*)'
+    title-format: '{team!upper}-{ticket!upper}: {postfix!normalize}'
+```
---
+++ index.ts
@@ -0,0 +1,83 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as format from 'string-format';
+import { graphql } from '@octokit/graphql';
+import { EventPayloads } from '@octokit/webhooks';
+import { titleCase } from 'title-case';
+
+async function run() {
+  try {
+    const token = process.env['GITHUB_TOKEN'];
+    if (!token) {
+      core.setFailed('GITHUB_TOKEN does not exist.');
+      return;
+    }
+
+    const graphqlWithAuth = graphql.defaults({
+      headers: { authorization: `token ${token}` },
+    });
+
+    if (github.context.eventName !== 'pull_request') {
+      return;
+    }
+
+    const branchRegex = new RegExp(core.getInput('branch-regex'), 'ig');
+    const titleFormat = core.getInput('title-format');
+
+    const payload = github.context
+      .payload as EventPayloads.WebhookPayloadPullRequest;
+
+    const pr = payload.pull_request;
+    console.log(`Running auto rename for #${pr.number} - ${pr.title}`);
+
+    const matches = branchRegex.exec(pr.head.ref);
+    if (!matches || !matches.groups) {
+      console.log(`${pr.head.ref} did not match ${branchRegex.source}`);
+      return;
+    }
+
+    // Create a formatter with custom transforms
+    const fmt = format.create({
+      upper: s => s.toUpperCase(),
+      normalize: s => titleCase(s.replace(/\W/g, ' ').trim()),
+    });
+
+    const formattedTitle = fmt(titleFormat, matches.groups);
+    if (!formattedTitle || !formattedTitle.trim()) {
+      console.log(
+        `${matches.groups} and ${titleFormat} resulted in an empty string`,
+      );
+      return;
+    }
+    console.log(`Renaming #${pr.number} to ${formattedTitle}`);
+
+    try {
+      const result = await graphqlWithAuth(
+        `
+        mutation updatePR($pullRequestId: ID!, $title: String) {
+          updatePullRequest(input: {pullRequestId: $pullRequestId, title: $title}) {
+            pullRequest {
+              title
+            }
+          }
+        }`,
+        {
+          pullRequestId: pr.node_id,
+          title: formattedTitle,
+        },
+      );
+
+      // Set this actions output to the matched groups
+      core.setOutput('matches', matches.groups);
+
+      const response = JSON.stringify(result, undefined, 2);
+      console.log(`The response payload: ${response}`);
+    } catch (error) {
+      console.log(`Request failed: ${JSON.stringify(error)}`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
